0d17a2ab4c9da5041987b4f6a9f37810b7dc3a38
Steffen Wagner <wagner@voyagersoft.de>
2026-05-06 19:30:14 +0200

    add stereo

--- 10_sound/10_sound.ino
+++ 10_sound/10_sound.ino
@@ -2,18 +2,21 @@
  * @brief Spielt den Star Wars Imperial March auf einem passiven Piezo-Lautsprecher.
  *
  * Verdrahtung (Beispiel):
- * - Piezo Signal -> D8
- * - Piezo GND    -> GND
+ * - Piezo links Signal  -> D8
+ * - Piezo rechts Signal -> D9
+ * - Beide Piezo GND     -> GND
  *
  * Hinweis:
- * - Das Beispiel nutzt die eingebaute Arduino-Funktion tone().
+ * - Das Beispiel erzeugt zwei Kanaele direkt auf D8 und D9.
  * - Ein passiver Piezo eignet sich besser als ein aktiver Summer.
  *
  * @author Steffen Wagner <steffen@swagner.de>
  */
 
-const int PIEZO_PIN = 8;
-const float TUNING_FACTOR = 1.5f;  // Bei Bedarf z.B. 0.98f oder 1.02f setzen.
+const int LEFT_PIEZO_PIN = 8;
+const int RIGHT_PIEZO_PIN = 9;
+const float TUNING_FACTOR = 1.00f;
+const int NOTE_GAP_DIVISOR = 4;
 
 const int NOTE_C4 = 262;
 const int NOTE_D4 = 294;
@@ -92,26 +95,76 @@ const NoteEvent score[] = {
 const int noteCount = sizeof(score) / sizeof(score[0]);
 
 
-void playNote(int frequency, int durationMs) {
-  if (frequency > 0) {
-    const int tunedFrequency = (int)(frequency * TUNING_FACTOR + 0.5f);
-    tone(PIEZO_PIN, tunedFrequency, durationMs);
+void playStereoTones(int leftFrequency, int rightFrequency, int durationMs) {
+  const unsigned long durationUs = (unsigned long)durationMs * 1000UL;
+  const unsigned long startUs = micros();
+  unsigned long lastToggleLeftUs = startUs;
+  unsigned long lastToggleRightUs = startUs;
+
+  bool leftState = false;
+  bool rightState = false;
+
+  const unsigned long leftHalfPeriodUs = leftFrequency > 0
+    ? 500000UL / (unsigned long)leftFrequency
+    : 0;
+  const unsigned long rightHalfPeriodUs = rightFrequency > 0
+    ? 500000UL / (unsigned long)rightFrequency
+    : 0;
+
+  digitalWrite(LEFT_PIEZO_PIN, LOW);
+  digitalWrite(RIGHT_PIEZO_PIN, LOW);
+
+  while ((micros() - startUs) < durationUs) {
+    const unsigned long nowUs = micros();
+
+    if (leftHalfPeriodUs > 0 && (nowUs - lastToggleLeftUs) >= leftHalfPeriodUs) {
+      leftState = !leftState;
+      digitalWrite(LEFT_PIEZO_PIN, leftState ? HIGH : LOW);
+      lastToggleLeftUs = nowUs;
+    }
+
+    if (rightHalfPeriodUs > 0 && (nowUs - lastToggleRightUs) >= rightHalfPeriodUs) {
+      rightState = !rightState;
+      digitalWrite(RIGHT_PIEZO_PIN, rightState ? HIGH : LOW);
+      lastToggleRightUs = nowUs;
+    }
   }
 
-  delay(durationMs);
-  noTone(PIEZO_PIN);
-  delay(durationMs / 4);
+  digitalWrite(LEFT_PIEZO_PIN, LOW);
+  digitalWrite(RIGHT_PIEZO_PIN, LOW);
+}
+
+
+void playNote(int frequency, int durationMs, int noteIndex) {
+  const int tunedFrequency = frequency > 0
+    ? (int)(frequency * TUNING_FACTOR + 0.5f)
+    : 0;
+  const int gapMs = durationMs / NOTE_GAP_DIVISOR;
+  const int playTimeMs = durationMs - gapMs;
+
+  if ((noteIndex % 2) == 0) {
+    playStereoTones(tunedFrequency, tunedFrequency > 0 ? tunedFrequency / 2 : 0, playTimeMs);
+  } else {
+    playStereoTones(tunedFrequency > 0 ? tunedFrequency / 2 : 0, tunedFrequency, playTimeMs);
+  }
+
+  digitalWrite(LEFT_PIEZO_PIN, LOW);
+  digitalWrite(RIGHT_PIEZO_PIN, LOW);
+  delay(gapMs);
 }
 
 
 void setup() {
-  pinMode(PIEZO_PIN, OUTPUT);
+  pinMode(LEFT_PIEZO_PIN, OUTPUT);
+  pinMode(RIGHT_PIEZO_PIN, OUTPUT);
+  digitalWrite(LEFT_PIEZO_PIN, LOW);
+  digitalWrite(RIGHT_PIEZO_PIN, LOW);
 }
 
 
 void loop() {
   for (int i = 0; i < noteCount; i++) {
-    playNote(score[i].frequency, score[i].durationMs);
+    playNote(score[i].frequency, score[i].durationMs, i);
   }
 
   delay(2000);
