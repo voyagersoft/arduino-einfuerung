80928deac94473b025143fd57bba4e45e2ba8ce5
Steffen Wagner <wagner@voyagersoft.de>
2026-05-08 15:52:00 +0200

    Add header

--- 10_sound/10_sound.ino
+++ 10_sound/10_sound.ino
@@ -45,52 +45,8 @@ struct NoteEvent {
   int durationMs;
 };
 
-const NoteEvent score[] = {
-  {NOTE_A4, 500},
-  {NOTE_A4, 500},
-  {NOTE_A4, 500},
-  {NOTE_F4, 350},
-  {NOTE_C5, 150},
-  {NOTE_A4, 500},
-  {NOTE_F4, 350},
-  {NOTE_C5, 150},
-  {NOTE_A4, 650},
-  {NOTE_E5, 500},
-  {NOTE_E5, 500},
-  {NOTE_E5, 500},
-  {NOTE_F5, 350},
-  {NOTE_C5, 150},
-  {NOTE_GS4, 500},
-  {NOTE_F4, 350},
-  {NOTE_C5, 150},
-  {NOTE_A4, 650},
-  {NOTE_A5, 500},
-  {NOTE_A4, 300},
-  {NOTE_A4, 150},
-  {NOTE_A5, 500},
-  {NOTE_GS5, 325},
-  {NOTE_G5, 175},
-  {NOTE_FS5, 125},
-  {NOTE_F5, 125},
-  {NOTE_FS5, 250},
-  {0, 325},
-  {NOTE_AS4, 250},
-  {NOTE_DS5, 500},
-  {NOTE_D5, 325},
-  {NOTE_CS5, 175},
-  {NOTE_C5, 125},
-  {NOTE_B4, 125},
-  {NOTE_C5, 250},
-  {0, 325},
-  {NOTE_F4, 250},
-  {NOTE_GS4, 500},
-  {NOTE_F4, 350},
-  {NOTE_A4, 125},
-  {NOTE_C5, 500},
-  {NOTE_A4, 375},
-  {NOTE_C5, 125},
-  {NOTE_E5, 650}
-};
+#include "imperial_march_stereo.h"
+
 
 const int noteCount = sizeof(score) / sizeof(score[0]);
 
